ba8049a4fb47d424489031639bcbb541c6f238cb
Ivan <yanciy_2@ukr.net>
2022-10-31 16:10:05 +0100

    Labka

---
+++ LB-3/Завдання 1.js
@@ -0,0 +1,4 @@
+var text = ['Ivan','Vasya','Dania'];
+for (var i = 0; i < text.length; i++) {
+    alert('Ширина елементів: '+text.length+'');
+}
---
+++ LB-3/Завдання 4.js
@@ -0,0 +1,43 @@
+//Створити функцію, яка за номером місяця повертає його назву.
+
+month = prompt('Введіть номер місяця');
+switch(month){
+    case '1':
+        alert('Січень');
+        break;
+    case '2':
+        alert('Лютий');
+        break;
+    case '3':
+        alert('Березень');
+        break;
+    case '4':
+        alert('Квітень');
+        break;
+    case '5':
+        alert('Травень');
+        break;
+    case '6':
+        alert('Червень');
+        break;
+    case '7':
+        alert('Липень');
+        break;
+    case '8':
+        alert('Серпень');
+        break;
+    case '9':
+        alert('Вересень');
+        break;
+    case '10':
+        alert('Жовтень');
+        break;
+    case '11':
+        alert('Листопад');
+        break;
+    case '12':
+        alert('Грудень');
+        break;                                               
+    default:
+        alert('Введіть число місяць від 1-12');   
+}    
